a9daa4ccd3681774ad44751f5c177a04c74e647a
Changbum97 <chb2005@naver.com>
2022-11-30 23:47:25 +0900

    First Commit

---
+++ src/main/java/com/lionheart15/ideaboard/IdeaBoardApplication.java
@@ -0,0 +1,13 @@
+package com.lionheart15.ideaboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeaBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lionheart15/ideaboard/IdeaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lionheart15.ideaboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
