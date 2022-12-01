c57a7d99d398f3af8710c8cad5630866c85d9ab0
pi koo <rnrudejr1234@naver.com>
2022-12-01 17:47:47 +0900

    엔티티 수정

--- src/main/java/com/lionheart15/ideaboard/domain/User.java
+++ src/main/java/com/lionheart15/ideaboard/domain/User.java
@@ -3,6 +3,8 @@ package com.lionheart15.ideaboard.domain;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @AllArgsConstructor
@@ -13,6 +15,7 @@ import javax.persistence.Id;
 @Entity
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String birth;
