174a5207905050e1a65cc250e154b4167908c083
nayeong kim <psi5059@naver.com>
2022-12-01 17:50:22 +0900

    comment dto

---
+++ src/main/java/com/lionheart15/ideaboard/domain/Comment.java
@@ -0,0 +1,28 @@
+package com.lionheart15.ideaboard.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+    private LocalDateTime createAt;
+    private Long userId;
+    private Long contentId;
+
+
+}
