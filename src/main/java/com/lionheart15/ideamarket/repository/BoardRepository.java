77b275121f9faaf5326267fc745070864c395e0c
pi koo <rnrudejr1234@naver.com>
2022-12-07 10:59:53 +0900

    좋아요 순 리스트화 진행중 1. Board,Good,User Repository 추가 (다른분 반영했다면 미반영해도됨) 2. Good Service 추가 3. 무스타치 main 출력 형식 추가

---
+++ src/main/java/com/lionheart15/ideamarket/repository/BoardRepository.java
@@ -0,0 +1,13 @@
+package com.lionheart15.ideamarket.repository;
+
+import com.lionheart15.ideamarket.domain.entity.Board;
+import com.lionheart15.ideamarket.domain.entity.Good;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board,Long> {
+}
---
+++ src/main/java/com/lionheart15/ideamarket/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.lionheart15.ideamarket.repository;
+
+import com.lionheart15.ideamarket.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
